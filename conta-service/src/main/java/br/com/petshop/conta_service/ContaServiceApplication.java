119e9f474275717e4eadd74e0c3834d45cbbac57
Isaac Newton <isaacnewton.montinegro@gmail.com>
2025-06-09 17:18:28 -0300

    contaservice fix

--- conta-service/src/main/java/br/com/petshop/conta_service/ContaServiceApplication.java
+++ conta-service/src/main/java/br/com/petshop/conta_service/ContaServiceApplication.java
@@ -2,12 +2,14 @@ package br.com.petshop.conta_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ContaServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ContaServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ContaServiceApplication.class, args);
+    }
 
 }
